7127cdb4f5c762df90b148f097af79aa1c12dfc3
Li Minghua <254713804@qq.com>
2016-12-15 22:55:18 +0800

    head完成
    head动画完成

--- js/index.js
+++ js/index.js
@@ -2,10 +2,30 @@ $(function(){
     $('#dowebok').fullpage({
         'verticalCentered': false,
         'css3': true,
-        'sectionsColor': ['#254875', '#00FF00', '#254587', '#695684','#ccc'],
+        'sectionsColor': ['#1889C5', '#9C58B6', '#26B1B6', '#129DE7','#ED6461'],
         anchors: ['page1', 'page2', 'page3', 'page4'],
         'navigation': true,
+        'scrollingSpeed':600,
+        'loopBottom':true,
+        'loopTop':true,
         'navigationPosition': 'right',
-        'navigationTooltips': ['1', '2', '3', '4','5']
+        'navigationTooltips': ['1', '2', '3', '4','5'],
+        afterLoad: function(anchorLink, index){
+            if (index === 1) {
+                
+            }
+            if (index === 2) {
+                
+            }
+            if (index === 3) {
+                
+            }
+            if (index === 4) {
+                
+            }
+            if (index === 5) {
+                
+            }
+        }
     });
 });
