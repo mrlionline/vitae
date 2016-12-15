5fc64baf02092098605b95e2c112181044481c08
Li Minghua <254713804@qq.com>
2016-12-15 18:13:53 +0800

    基础搭建完成
    滚动基础完成，差添加内容

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('#dowebok').fullpage({
+        'verticalCentered': false,
+        'css3': true,
+        'sectionsColor': ['#254875', '#00FF00', '#254587', '#695684','#ccc'],
+        anchors: ['page1', 'page2', 'page3', 'page4'],
+        'navigation': true,
+        'navigationPosition': 'right',
+        'navigationTooltips': ['1', '2', '3', '4','5']
+    });
+});
